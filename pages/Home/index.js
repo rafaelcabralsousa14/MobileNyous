9a9c80f589ffcc37aec1218cc9b9ed2f4e3b5e16
rafaelcabralsousa14 <61585019+rafaelcabralsousa14@users.noreply.github.com>
2020-11-16 10:15:23 -0300

    Listagem de Eventos

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,24 +1,50 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage'
 
 const Home = () => {
 
-    const [token, setToken] = useState('');
+    // const [token, setToken] = useState('');
 
-    const getToken = async () => {
-        setToken(await AsyncStorage.getItem('@jwt'))
+    // const getToken = async () => {
+    //     setToken(await AsyncStorage.getItem('@jwt'))
+    // }
+
+    // useEffect(()=>{
+    //     getToken();
+    // }, [])      
+
+    const [eventos, setEventos] = useState([])
+
+    useEffect(() => {
+        listarEventos();
+    }, [])
+
+    const listarEventos = () => {
+        fetch('http://192.168.0.6:5000/api/eventos')
+        .then(response => response.json())
+        .then(dados => {
+            setEventos(dados.data);
+        })
+        .catch(err => console.error(err));
     }
 
-    useEffect(()=>{
-        getToken();
-    }, []) 
+    const renderItem = ({evento}) => {
+        return(
+            <ItemEvento nome={evento.item.nome} imagem={evento.item.urlImagem} link={evento.item.link} />
+        )
+    }
 
     return(
         <View>
             <Text>Home</Text>
-            <Text>{token}</Text>
+            {/* <Text>{token}</Text> */}
+            <FlatList
+                data={eventos}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
         </View>
     )
 }
